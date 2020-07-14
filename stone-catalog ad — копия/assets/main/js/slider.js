9c7203dcf6c84164bdf32c48869aa171b7c3c6b0
prostacoder <68264747+prostacoder@users.noreply.github.com>
2020-07-14 08:02:43 +0300

    Stone-1
    stone-1

---
+++ stone-catalog ad — копия/assets/main/js/slider.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+jQuery(document).ready(function() {
+   
+
+$('.main__projects_sliderк').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 4
+  });
+  });
+
+
+
